aa94020516beeb82f66e2b72139383cbe9f35835
Sinigami <Sinigami@DESKTOP-E37HA8N>
2017-03-30 15:05:11 +0300

    Версия 2.1 рабочая без дин. памяти

--- Main/Main/Main.cpp
+++ Main/Main/Main.cpp
@@ -1,96 +1,99 @@
-//не пашущий хз почему алгоритм (не делаетс€ проход по всем соседним €чейкам) 
-
+//working algorithm without dynamic memory
+#include <string>
 #include "stdafx.h"
 #include <iostream>
+#include <cstring>
+#include <cstdio>
 #include <conio.h>
 #include <stdlib.h>
 using namespace std;
-const int length = 15, width = 10;
-int Array[length][width], d, i, j, WayX[length * width], WayY[length * width], len = 0;
+const int length = 10, width = 10;
+int Array[length][width], d, i, j, WayRow[length * width], WayColumn[length * width], len = 0;
 const int WALL = -2, PASS = -1;
 
-int EnterCoordinates(char variable) {
-	cout << variable << " is ";
+int EnterCoordinates(char* variable) {
+	puts(variable);
+	cout << " is ";
 	int input;
 	cin >> input;
-	return input - 1;} //потому что нумераци€ элементов массива с 0, а не 1
+	return input - 1;
+}
 
-bool findWay(int x_start, int y_start, int x_finish, int y_finish) {
-	int x_displacement[4] = {1, 0, -1, 0}, 
-	int y_displacement[4] = {0, 1, 0, -1};
-	int d, x, y, k;
-	bool what = true, stop;
+bool findWay(int row_start, int column_start, int row_finish, int column_finish) {
+	int row_displacement[4] = { 1, 0, -1, 0 };
+	int column_displacement[4] = { 0, 1, 0, -1 };
+	int d = 0, row, column, k;
+	bool stop;
 
-/*	if ((Array[y_start][x_start] == WALL) || (Array[y_finish][x_finish] == WALL))
-		what = false;*/
+	if ((Array[row_start][column_start] == WALL) || (Array[row_finish][column_finish] == WALL))
+		return false;
 
-	d = 0;
-	Array[y_start][x_start] = 0;
+	Array[row_start][column_start] = 0;
 	do {
-		stop = true;
-		for (y = 0; y < length; ++y) {
-			for (x = 0; x < width; ++x) {
-				if (Array[y][x] == d) {
-					for (k = 0; k < 4; ++k) {
-						int iy = y + y_displacement[k], ix = x + x_displacement[k];
-						if ((iy >= 0) && (iy < length) && (ix >= 0) && (ix < width) && (Array[iy][ix] == PASS)) {
+		stop = true; //по умолчанию считаем, что повтор€ть еще раз алгоритм не нужно
+		for (row = 0; row < length; row++) { //проходим по каждой строчке
+			for (column = 0; column < width; column++) { //в ней - по каждой колонке
+				if (Array[row][column] == d) { //
+					for (k = 0; k < 4; k++) {
+						int icolumn = column + column_displacement[k], irow = row + row_displacement[k];
+						if ((icolumn >= 0) && (icolumn < width) && (irow >= 0) && (irow < length) && (Array[irow][icolumn] == PASS)) {
 							stop = false;
-							Array[iy][ix] = d + 1;
+							Array[irow][icolumn] = d + 1;
 						}
+					}
 				}
 			}
 		}
 		d++;
-		for (i = 0; i < length; i++) {
-			for (j = 0; j < width; j++) {
-				cout << Array[i][j] << "\t";
-			}
-			cout << endl;
+	} while ((stop) || (Array[row_finish][column_finish] == PASS));
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < width; j++) {
+			cout << Array[i][j] << "\t";
 		}
-		_getch();
+		cout << endl;
 	}
 
-	while ((!stop) && (Array[y_finish][x_finish] == PASS));
+	_getch();
 	system("cls");
 
-	cout << Array[y_finish][x_finish];
-	_getch();
-	if (Array[y_finish][x_finish] == PASS)
-		what = false;
+	if (Array[row_finish][column_finish] == PASS)
+		return false;
 
-	len = Array[y_finish][x_finish];
-	x = x_finish;
-	y = y_finish;
+	len = Array[row_finish][column_finish];
 	d = len;
+	WayRow[d] = row_finish;
+	WayColumn[d] = column_finish;
 	while (d > 0) {
-		WayX[d] = x;
-		WayY[d] = y;
 		d--;
 		for (k = 0; k < 4; ++k) {
-			int iy = y + y_displacement[k], ix = x + x_displacement[k];
-			if ((iy >= 0) && (iy < length) && (ix >= 0) && (ix < width) && (Array[iy][ix] == d)) {
-				x = x + x_displacement[k];
-				y = y + y_displacement[k];
+			int icolumn = WayColumn[d + 1] + column_displacement[k], irow = WayRow[d + 1] + row_displacement[k];
+			if ((icolumn >= 0) && (icolumn < width) && (irow >= 0) && (irow < length) && (Array[irow][icolumn] == d)) {
+				WayRow[d] = irow;
+				WayColumn[d] = icolumn;
 				break;
 			}
 		}
 	}
-	WayX[0] = x_start;
-	WayY[0] = y_start;
-	return what;
+	WayRow[0] = row_start;
+	WayColumn[0] = column_start;
+
+	for (int i = 0; i <= len; i++) {
+		cout << "WayRow = " << WayRow[i] << " WayColumn = " << WayColumn[i] << endl;
+	}
+	return true;
 }
 
-int _tmain()
+int main()
 {
 	int d = 0;
-	int xstart, ystart, xfinish, yfinish;
+	int rowstart, columnstart, rowfinish, columnfinish;
 	cout << "Enter the coordinates of the start:\n";
-	xstart = EnterCoordinates('x');
-	ystart = EnterCoordinates('y');
+	rowstart = EnterCoordinates("row");
+	columnstart = EnterCoordinates("column");
 	system("cls");
 	cout << "Enter the coordinates of the finish:\n";
-	xfinish = EnterCoordinates('x');
-	yfinish = EnterCoordinates('y');
+	rowfinish = EnterCoordinates("row");
+	columnfinish = EnterCoordinates("column");
 
 	for (i = 0; i < length; i++) {
 		for (j = 0; j < width; j++) {
@@ -98,37 +101,45 @@ int _tmain()
 		}
 	}
 
-	/*char choice = '1';
+	char choice = '1';
 	while (choice == '1') {
+		system("cls");
+		cout << "Enter the coordinate of wall, please:\n";
+		int row = EnterCoordinates("row");
+		int column = EnterCoordinates("column");
+		Array[row][column] = WALL;
+		system("cls");
+		cout << "Do you want to add a new wall?\n1. Yes.\n2. No.\n";
+		choice = _getch();
+	}
 	system("cls");
-	cout << "Enter the coordinate of wall, please:\n";
-	int x = EnterCoordinates('x');
-	int y = EnterCoordinates('y');
-	Array[x][y] = WALL;
-	system("cls");
-	cout << "Do you want to add a new wall?\n1. Yes.\n2. No.\n";
-	choice = _getch();}*/
-
-	if (findWay(xstart, ystart, xfinish, yfinish) == true) {
+	if (findWay(rowstart, columnstart, rowfinish, columnfinish) == true) {
 		for (i = 0; i < length; i++) {
 			for (j = 0; j < width; j++) {
-				if (Array[i][j] == 0)
-					cout << "START\t";
-				else {
-					if ((i == xfinish) && (j == yfinish))
-						cout << "FINISH\t";
-					else {
-						for (int p = 0; p < len; p++) {
-							if ((i == WayX[p]) && (j == WayY[p])) {
-								cout << "H\t";
-							}
-							else
-								cout << " \t";
-						};
-					}
-					cout << endl;
+				if (Array[i][j] == 0) {
+					cout << "S ";
 				}
-			}
+				else if (Array[i][j] == WALL) {
+					cout << "  ";
+				}
+				else {
+					if ((i == rowfinish) && (j == columnfinish)) {
+						cout << "F ";
+					}
+					else {
+						bool amount = false;
+						for (int p = 0; p < len; p++) {
+							if ((i == WayRow[p]) && (j == WayColumn[p])) {
+								cout << "H ";
+								amount = true;
+							}
+						}
+						if (amount == false) {
+							cout << ". ";
+						}
+					}
+				}
+			}cout << endl;
 		}
 	}
 	else {
